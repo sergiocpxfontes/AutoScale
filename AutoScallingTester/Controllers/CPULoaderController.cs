267730f152b2287e0228e02bf4f7ba1ccdc86a1d
sergiocpxfontes <sergio@computencial.com>
2025-02-19 15:35:22 +0000

    Auto Scaling start

---
+++ AutoScallingTester/Controllers/CPULoaderController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoScalingTester.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CPULoaderController : Controller
+    {
+        public IActionResult CPULoader(int seconds,int percentage)
+        {
+            SimularCPUWorkLoad(seconds, percentage);
+            return Ok($"simulação de {percentage} % durante {seconds} segundos");
+        }
+        private void SimularCPUWorkLoad(int seconds,int percentage)
+        {
+            percentage = Math.Max(100, percentage);
+            var end = DateTime.Now.AddSeconds(seconds);
+
+            Stopwatch watch = new Stopwatch();
+
+            watch.Start();
+            while(DateTime.Now<end)
+            {
+                if(watch.ElapsedMilliseconds>percentage)
+                {
+                    System.Threading.Thread.Sleep(100 - percentage);
+                    watch.Restart();
+                }
+            }
+        }
+    }
+}
---
+++ AutoScallingTesterClient/Program.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+
+namespace AutoScallingTester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A iniciar");
+            for (int i = 0; i < 20; i++)
+            {
+                try
+                {
+                    Console.WriteLine(DateTime.Now.ToString("mm:ss") +  " - /api/CPULoader?percentage=70&seconds=80");
+                    using (WebClient webClient = new WebClient())
+                    {
+                        webClient.BaseAddress = "https://autoscaletest.azurewebsites.net";
+                        var json = webClient.DownloadString("/api/CPULoader?percentage=85&seconds=80");
+                    }
+                }
+                catch (WebException ex)
+                {
+                    throw ex;
+                }
+            }
+            Console.WriteLine("Concluído");
+            Console.ReadLine();
+        }
+
+    }
+}
